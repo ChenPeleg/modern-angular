711969eccfc47c9e0e986aa5aabf72c1eecfdfe5
chenp <chenpeleg@gmail.com>
2023-06-09 11:22:55 +0300

    removed all

--- src/app/emulation/runtime-exception.ts
+++
@@ -1,8 +0,0 @@
-export class RuntimeException extends Error
-{
-    constructor(public override message: string = "")
-    {
-        super(message);
-        Object.setPrototypeOf(this, RuntimeException.prototype);
-    }
-}
--- src/spec/tests.js
+++
@@ -1,3 +0,0 @@
-require('./alu.spec.ts');
-require('./assembler.spec.ts');
-require('./instructions.spec.ts');
